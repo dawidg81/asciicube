f27a195d2fb611b2cd354f5cd40a6194a56a78d7
dawidg81 <dawidgorski.m@gmail.com>
2025-12-07 17:27:51 +0100

    More line breaks and unknown command handling

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 	cout << "Merry Christmas! Welcome to AsciiCube.\n"
-		<< "Type 'help' for more information.\n";
+		<< "Type 'help' for more information.\n\n";
 
 	while(true){
 		string input;
@@ -12,18 +12,20 @@ int main(){
 		cin >> input;
 
 		if(input == "help"){
-			cout << "Hello, this is help. I will give you commands.\n"
+			cout << "Hello, this is help. I will give you commands.\n\n"
 				<< "play - start a local singleplayer game.\n"
 				<< "peerplay <address> - directly connects to an existing multiplayer game on the given address.\n"
 				<< "                     If address is not given, starts a new multiplayer game with operator status.\n"
 				<< "list - connects to a server that sends a list of currently online peer-nets.\n"
 				<< "about - Shows more information about this program.\n"
-				<< "quit - gracefully quits the program.\n";
+				<< "quit - gracefully quits the program.\n\n";
 		} else if(input == "about"){
 			cout << "This is AsciiCube v0.0.0 made in Poland by dawidg81 on 2025-12-07 (YYYY-MM-DD).\n";
 		} else if(input == "quit"){
 			cout << "Bye!\n";
 			return 0;
+		} else {
+			cout << "That command is not known to the command handler. If you think that it's a bug, please tell the developers!\n";
 		}
 	}
 
